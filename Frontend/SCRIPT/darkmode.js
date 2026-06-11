688d362eee5ada708d0834ddc81586e3a2212927
Beldevie-Bat <messiebatantou@gmail.com>
2026-06-11 22:19:04 +0100

    database et page de connexion

---
+++ Frontend/SCRIPT/darkmode.js
@@ -0,0 +1,41 @@
+
+const themeSauvegarde = localStorage.getItem('sp-theme') || 'dark';
+document.documentElement.setAttribute('data-theme', themeSauvegarde);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const boutonTheme = document.getElementById('theme-toggle');
+    const iconeTheme = document.getElementById('theme-icon');
+
+
+
+    //  icône au chargement de la page
+    if (document.documentElement.getAttribute('data-theme') === 'dark') {
+        iconeTheme.className = 'fa-solid fa-sun';
+    } else {
+        iconeTheme.className = 'fa-solid fa-moon';
+    }
+
+    
+
+    if (boutonTheme && iconeTheme) {
+        boutonTheme.addEventListener('click', () => {
+            const themeActuel = document.documentElement.getAttribute('data-theme');
+            
+            if (themeActuel === 'dark') {
+
+
+                // mode clair
+                document.documentElement.setAttribute('data-theme', 'light');
+                localStorage.setItem('sp-theme', 'light');
+                iconeTheme.className = 'fa-solid fa-moon';
+            } else {
+
+
+                // mode sombre
+                document.documentElement.setAttribute('data-theme', 'dark');
+                localStorage.setItem('sp-theme', 'dark');
+                iconeTheme.className = 'fa-solid fa-sun';
+            }
+        });
+    }
+});
---
+++ backend/secure.sql
@@ -0,0 +1,267 @@
+-- Table: public.commissariats
+
+-- DROP TABLE IF EXISTS public.commissariats;
+
+CREATE TABLE IF NOT EXISTS public.commissariats
+(
+    id_commissariat integer NOT NULL DEFAULT nextval('commissariats_id_commissariat_seq'::regclass),
+    nom character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    adresse text COLLATE pg_catalog."default",
+    telephone character varying(20) COLLATE pg_catalog."default",
+    CONSTRAINT commissariats_pkey PRIMARY KEY (id_commissariat)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.commissariats
+    OWNER to postgres;
+
+
+	
+
+-- Table: public.users
+
+-- DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id_user integer NOT NULL DEFAULT nextval('users_id_user_seq'::regclass),
+    nom character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    prenom character varying(100) COLLATE pg_catalog."default",
+    email character varying(150) COLLATE pg_catalog."default",
+    mot_de_passe character varying(255) COLLATE pg_catalog."default",
+    role character varying(30) COLLATE pg_catalog."default",
+    matricule character varying COLLATE pg_catalog."default",
+    telephone character varying COLLATE pg_catalog."default",
+    id_grade integer,
+    id_workspace integer,
+    id_commissariat integer,
+    CONSTRAINT users_pkey PRIMARY KEY (id_user),
+    CONSTRAINT users_email_key UNIQUE (email),
+    CONSTRAINT users_id_commissariat_fkey FOREIGN KEY (id_commissariat)
+        REFERENCES public.commissariats (id_commissariat) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT users_id_grade_fkey FOREIGN KEY (id_grade)
+        REFERENCES public.grades (id_grade) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT users_id_workspace_fkey FOREIGN KEY (id_workspace)
+        REFERENCES public.workspaces (id_workspace) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
+	
+
+
+-- Table: public.individus
+
+-- DROP TABLE IF EXISTS public.individus;
+
+CREATE TABLE IF NOT EXISTS public.individus
+(
+    id_individu integer NOT NULL DEFAULT nextval('individus_id_individu_seq'::regclass),
+    nom character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    prenom character varying(100) COLLATE pg_catalog."default",
+    telephone character varying(20) COLLATE pg_catalog."default",
+    adresse text COLLATE pg_catalog."default",
+    CONSTRAINT individus_pkey PRIMARY KEY (id_individu)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.individus
+    OWNER to postgres;
+
+
+	-- Table: public.workspaces
+
+-- DROP TABLE IF EXISTS public.workspaces;
+
+CREATE TABLE IF NOT EXISTS public.workspaces
+(
+    id_workspace integer NOT NULL DEFAULT nextval('workspaces_id_workspace_seq'::regclass),
+    nom character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    date_creation timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id_commissariat integer,
+    type_brigade character varying COLLATE pg_catalog."default",
+    CONSTRAINT workspaces_pkey PRIMARY KEY (id_workspace),
+    CONSTRAINT workspaces_id_commissariat_fkey FOREIGN KEY (id_commissariat)
+        REFERENCES public.commissariats (id_commissariat) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.workspaces
+    OWNER to postgres;
+
+
+	-- Table: public.grades
+
+-- DROP TABLE IF EXISTS public.grades;
+
+CREATE TABLE IF NOT EXISTS public.grades
+(
+    id_grade integer NOT NULL DEFAULT nextval('grades_id_grade_seq'::regclass),
+    libelle character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT grades_pkey PRIMARY KEY (id_grade)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.grades
+    OWNER to postgres;
+
+
+
+
+-- Table: public.enquête 
+
+-- DROP TABLE IF EXISTS public."enquête ";
+
+CREATE TABLE IF NOT EXISTS public."enquête "
+(
+    id_enquete integer NOT NULL DEFAULT nextval('"enquête _id_enquete_seq"'::regclass),
+    date_cloture date,
+    date_ouverture date,
+    "verdict " text COLLATE pg_catalog."default",
+    CONSTRAINT "enquête _pkey" PRIMARY KEY (id_enquete)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."enquête "
+    OWNER to postgres;
+
+	
+
+
+
+
+-- Table: public.garde_a_vue
+
+-- DROP TABLE IF EXISTS public.garde_a_vue;
+
+CREATE TABLE IF NOT EXISTS public.garde_a_vue
+(
+    id_garde_vue integer NOT NULL DEFAULT nextval('garde_a_vue_id_garde_vue_seq'::regclass),
+    date_debut date,
+    date_fin date,
+    cellule character varying COLLATE pg_catalog."default",
+    id_plainte integer NOT NULL DEFAULT nextval('garde_a_vue_id_plainte_seq'::regclass),
+    CONSTRAINT garde_a_vue_pkey PRIMARY KEY (id_garde_vue),
+    CONSTRAINT garde_a_vue_id_plainte_fkey FOREIGN KEY (id_plainte)
+        REFERENCES public.plaintes (id_plainte) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.garde_a_vue
+    OWNER to postgres;
+	
+
+
+	
+
+
+
+CREATE TABLE actes (
+    id_acte SERIAL PRIMARY KEY,
+    numero_acte VARCHAR(50) UNIQUE,
+    type_acte VARCHAR(30) NOT NULL,
+    description TEXT,
+    date_faits DATE,
+    heure_faits TIME,
+    quartier VARCHAR(100),
+    urgence VARCHAR(20),
+    statut VARCHAR(50),
+    id_user INTEGER REFERENCES users(id_user),
+    id_workspace INTEGER REFERENCES workspaces(id_workspace)
+);
+
+
+
+CREATE TABLE main_courantes (
+    id_maincourante SERIAL PRIMARY KEY,
+    date_debut DATE,
+    quatier VARCHAR,
+    id_acte INTEGER UNIQUE REFERENCES actes(id_acte)
+);
+    
+
+CREATE TABLE plaintes (
+    id_plainte SERIAL PRIMARY KEY,
+    qualification_penale VARCHAR(200),
+   date_depot DATE,
+  description TEXT,
+  nom_victime VARCHAR,
+  nom_suspect VARCHAR (50),
+  gravite_plainte VARCHAR,
+    id_act INTEGER UNIQUE REFERENCES actes(id_acte)
+);
+
+
+CREATE TABLE proces_verbaux (
+    id_pro SERIAL PRIMARY KEY,
+	date_audition DATE,
+	nom_entendu VARCHAR,
+    type_audition VARCHAR(100),
+	id_user INTEGER UNIQUE REFERENCES users(id_user),
+
+    id_acte INTEGER UNIQUE REFERENCES actes(id_acte)
+);
+
+
+
+CREATE TABLE audit_logs (
+    id_log SERIAL PRIMARY KEY,
+    type_action VARCHAR(100),
+    ancienne_valeur TEXT,
+    nouvelle_valeur TEXT,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    id_user INTEGER REFERENCES users(id_user)
+
+	
+);
+
+
+
+-- Table: public.enquête 
+
+-- DROP TABLE IF EXISTS public."enquête ";
+
+CREATE TABLE IF NOT EXISTS public."enquête "
+(
+    id_enquete integer NOT NULL DEFAULT nextval('"enquête _id_enquete_seq"'::regclass),
+    date_cloture date,
+    date_ouverture date,
+    "verdict " text COLLATE pg_catalog."default",
+    CONSTRAINT "enquête _pkey" PRIMARY KEY (id_enquete)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."enquête "
+    OWNER to postgres;
+
+
+
+
+
+
+
+
+
+
+
